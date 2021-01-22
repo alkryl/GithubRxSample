859d9a3ee39839543df303039c1fbf67031721ce
Aleksandr Krylov <aleksandr.krilov@loylabs.ru>
2021-01-22 10:46:36 +0300

    Added an extension with empty values of types

---
+++ GithubRxSample/Extensions/InitialValues.swift
@@ -0,0 +1,21 @@
+//
+//  InitialValues.swift
+//  GithubRxSample
+//
+//  Created by Alexander Krylov on 25.12.2020.
+//  Copyright © 2020 Alexander Krylov. All rights reserved.
+//
+
+import Foundation
+
+extension Int {
+    static let zero = 0
+}
+
+extension String {
+    static let empty = ""
+}
+
+extension Data {
+    static let empty = Data()
+}
