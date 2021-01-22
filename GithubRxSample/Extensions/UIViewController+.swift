c93488651787db313f27c8eacd090d4c81e02c11
Aleksandr Krylov <aleksandr.krilov@loylabs.ru>
2021-01-22 10:48:02 +0300

    Added some extensions for UIViewController (hideBar and showAlert)

---
+++ GithubRxSample/Extensions/UIViewController+.swift
@@ -0,0 +1,15 @@
+//
+//  UIViewController+.swift
+//  GithubRxSample
+//
+//  Created by Alexander Krylov on 25.12.2020.
+//  Copyright © 2020 Alexander Krylov. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    func hideNavigationBar(_ hide: Bool, animated: Bool = true) {
+        navigationController?.setNavigationBarHidden(hide, animated: animated)
+    }
+}
--- GithubRxSample/Extensions/UIViewController+RxAlert.swift
+++ GithubRxSample/Extensions/UIViewController+RxAlert.swift
@@ -9,8 +9,9 @@
 import RxSwift
 
 extension UIViewController {
-    func showAlert(title: String, description: String?,
-                   closureToExecute: (() -> ())?) -> Observable<Void> {
+    func showAlert(title: String = .empty,
+                   description: String?,
+                   closureToExecute: (() -> ())? = nil) -> Observable<Void> {
         return Observable.create({ [weak self] observer in
             let alert = UIAlertController(title: title,
                                           message: description,
@@ -19,16 +20,7 @@ extension UIViewController {
                 alert.dismiss(animated: true, completion: nil)
                 observer.onCompleted()
             })
-            let showAction = UIAlertAction(title: "Show", style: .default, handler: { _ in
-                if let closure = closureToExecute {
-                    closure()
-                } else {
-                    alert.dismiss(animated: true, completion: nil)
-                }
-                observer.onCompleted()
-            })
             alert.addAction(closeAction)
-            alert.addAction(showAction)
             self?.present(alert, animated: true, completion: nil)
             return Disposables.create()
         })
