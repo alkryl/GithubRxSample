997255e2bb32e47010d1635a44dc15f846227b23
Aleksandr Krylov <aleksandr.krilov@loylabs.ru>
2021-01-26 11:15:32 +0300

    Commit viewModels were updated

---
+++ GithubRxSample/Commit/ViewModel/CommitCellViewModel.swift
@@ -0,0 +1,61 @@
+//
+//  CommitCellViewModel.swift
+//  GithubRxSample
+//
+//  Created by Alexander Krylov on 19/06/2020.
+//  Copyright © 2020 Alexander Krylov. All rights reserved.
+//
+
+struct CommitCellViewModel {
+    
+    private var commit: CommitItem
+    
+    //MARK: Initialization
+    
+    init(_ commit: CommitItem) {
+        self.commit = commit
+    }
+}
+
+//MARK: Computed properties
+
+extension CommitCellViewModel {
+    private var message: String {
+        return (commit.commit?.message).orEmpty
+    }
+    
+    private var avatar: String {
+        return (commit.author?.avatar).orEmpty
+    }
+    
+    private var login: String {
+        return (commit.author?.login).orEmpty
+    }
+    
+    private var date: String {
+        return (commit.commit?.author?.date).orEmpty
+    }
+    
+    private var hash: String {
+        return (commit.hash).orEmpty
+    }
+}
+
+//MARK: Methods
+
+extension CommitCellViewModel {
+    private enum Tag: Int {
+        case message = 0, avatar, login, date, hash
+    }
+    
+    func parameter(for tag: Int) -> AnyHashable? {
+        switch Tag(rawValue: tag) {
+        case .message: return message
+        case .avatar:  return avatar
+        case .login:   return login
+        case .date:    return date
+        case .hash:    return hash
+        default:       return nil
+        }
+    }
+}
---
+++ GithubRxSample/Commit/ViewModel/CommitsViewModel.swift
@@ -0,0 +1,109 @@
+//
+//  CommitsViewModel.swift
+//  GithubRxSample
+//
+//  Created by Alexander Krylov on 26/05/2020.
+//  Copyright © 2020 Alexander Krylov. All rights reserved.
+//
+
+import RxSwift
+import RxCocoa
+import Moya
+import Moya_ObjectMapper
+
+final class CommitsViewModel: ViewModelProtocol {
+    
+    //MARK: Rx
+    
+    private let db = DisposeBag()
+    
+    private(set) var dataSections = BehaviorRelay<[CommitSection]>(value: [])
+    private(set) var selectedIndexPath = PublishSubject<IndexPath>()
+    private(set) var dataRelay = BehaviorRelay(value: [CommitItem]())
+    private(set) var hashSubject = PublishSubject<String>()
+    private(set) var errorSubject = PublishSubject<SampleError>()
+    
+    //MARK: Properties
+    
+    private var commits = [CommitItem]()
+    private(set) var repository: String
+        
+    var provider: MoyaProvider<APIClient> = {
+        let configuration = NetworkLoggerPlugin.Configuration(logOptions: [.requestMethod])
+        let plugin = NetworkLoggerPlugin(configuration: configuration)
+        let provider = MoyaProvider<APIClient>(plugins: [plugin])
+        return provider
+    }()
+        
+    //MARK: Initialization
+    
+    init(repository: String) {
+        self.repository = repository
+        subscribe()
+    }
+    
+    //MARK: Methods
+    
+    func updatePath(_ path: IndexPath) {
+        selectedIndexPath.onNext(path)
+    }
+    
+    func showError(_ type: SampleError) {
+        errorSubject.onNext(type)
+    }
+    
+    //MARK: Handlers
+    
+    private func didObtainCommits(_ items: [CommitItem]) {
+        commits.append(contentsOf: items)
+        let sections = CommitSection(header: .empty, items: commits)
+        dataSections.accept([sections])
+    }
+    
+    private func didObtainError(_ type: SampleError) {
+        showError(type)
+    }
+}
+
+//MARK: Computed properties
+
+extension CommitsViewModel {
+    var title: Driver<String> {
+        return Driver.just(repository)
+            .map { $0.components(separatedBy: "/").last }
+            .unwrap()
+    }
+}
+
+//MARK: Subscriber
+
+extension CommitsViewModel: Subscriber {
+    func subscribe() {
+        Observable<String>.just(.empty)
+            .flatMap { _ in self.provider.rx.request(.commits(self.repository)) }
+            .mapArray(CommitItem.self)
+            .catchError { [weak self] error in
+                self?.didObtainError(.mapping(reason: error.localizedDescription))
+                return Observable.empty()
+            }
+            .subscribe { [weak self] event in
+                switch event {
+                case .next(let commits):
+                    guard let self = self else { return }
+                    self.didObtainCommits(commits)
+                case .error(let error):
+                    self?.didObtainError(.obtaining(reason: error.localizedDescription))
+                default: break
+                }
+            }
+            .disposed(by: db)
+        
+        selectedIndexPath
+            .map { $0.row }
+            .subscribe(onNext: { [weak self] row in
+                let hash = (self?.commits[row].hash).orEmpty
+                self?.hashSubject.onNext(hash)
+            })
+            .disposed(by: db)
+    }
+}
