e1c96a2de7397e07fbdf5b6311ddcc02de942a6a
Aleksandr Krylov <aleksandr.krilov@loylabs.ru>
2021-01-26 11:12:43 +0300

    Changed CommitItem model for ObjectMapper

---
+++ GithubRxSample/Commit/Model/Commit.swift
@@ -0,0 +1,57 @@
+//
+//  Commit.swift
+//  GithubRxSample
+//
+//  Created by Alexander Krylov on 05/06/2020.
+//  Copyright © 2020 Alexander Krylov. All rights reserved.
+//
+
+import ObjectMapper
+
+struct CommitItem: Mappable {
+    var hash: String?
+    var commit: Commit?
+    var author: Author?
+    
+    init?(map: Map) { }
+    
+    mutating func mapping(map: Map) {
+        hash   <- map["sha"]
+        commit <- map["commit"]
+        author <- map["author"]
+    }
+}
+
+struct Commit: Mappable {
+    var message: String?
+    var author: AuthorInfo?
+    
+    init?(map: Map) { }
+    
+    mutating func mapping(map: Map) {
+        message <- map["message"]
+        author  <- map["author"]
+    }
+}
+
+struct Author: Mappable {
+    var login: String?
+    var avatar: String?
+    
+    init?(map: Map) { }
+    
+    mutating func mapping(map: Map) {
+        login  <- map["login"]
+        avatar <- map["avatar_url"]
+    }
+}
+
+struct AuthorInfo: Mappable {
+    var date: String?
+    
+    init?(map: Map) { }
+    
+    mutating func mapping(map: Map) {
+        date <- map["date"]
+    }
+}
