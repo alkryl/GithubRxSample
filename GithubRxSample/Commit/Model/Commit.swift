5214e3addd6b37ed59861e3e0d8168327c869874
Aleksandr Krylov <aleksandr.krilov@loylabs.ru>
2021-01-26 11:17:26 +0300

    Minor changes of CommitItem model

--- GithubRxSample/Commit/Model/Commit.swift
+++ GithubRxSample/Commit/Model/Commit.swift
@@ -22,36 +22,38 @@ struct CommitItem: Mappable {
     }
 }
 
-struct Commit: Mappable {
-    var message: String?
-    var author: AuthorInfo?
-    
-    init?(map: Map) { }
-    
-    mutating func mapping(map: Map) {
-        message <- map["message"]
-        author  <- map["author"]
+extension CommitItem {
+    struct Commit: Mappable {
+        var message: String?
+        var author: AuthorInfo?
+        
+        init?(map: Map) { }
+        
+        mutating func mapping(map: Map) {
+            message <- map["message"]
+            author  <- map["author"]
+        }
     }
-}
 
-struct Author: Mappable {
-    var login: String?
-    var avatar: String?
-    
-    init?(map: Map) { }
-    
-    mutating func mapping(map: Map) {
-        login  <- map["login"]
-        avatar <- map["avatar_url"]
+    struct Author: Mappable {
+        var login: String?
+        var avatar: String?
+        
+        init?(map: Map) { }
+        
+        mutating func mapping(map: Map) {
+            login  <- map["login"]
+            avatar <- map["avatar_url"]
+        }
     }
-}
 
-struct AuthorInfo: Mappable {
-    var date: String?
-    
-    init?(map: Map) { }
-    
-    mutating func mapping(map: Map) {
-        date <- map["date"]
+    struct AuthorInfo: Mappable {
+        var date: String?
+        
+        init?(map: Map) { }
+        
+        mutating func mapping(map: Map) {
+            date <- map["date"]
+        }
     }
 }
